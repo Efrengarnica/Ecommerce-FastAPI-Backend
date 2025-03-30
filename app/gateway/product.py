b6e244244109b84994f1fc7f9cfffa63efb3102d
Efren Garnica <efrengarnicapalacios@gmail.com>
2025-03-29 21:41:33 -0600

    Se eliminan las sessiones pasadas como dependencias.

--- app/gateway/product.py
+++ app/gateway/product.py
@@ -1,32 +1,31 @@
 from fastapi import HTTPException
 from app.models.product import Product, ProductPatch
-from sqlmodel import Session
 from app.repository.product import ProductRepository
 
 class ProductGateway:
     
     @classmethod
-    def create_product(cls, product: Product, session: Session) -> Product:
-        return ProductRepository.create_product(product, session)
+    def create_product(cls, product: Product) -> Product:
+        return ProductRepository.create_product(product)
 
     @classmethod
-    def get_products(cls, session: Session) -> list[Product]:
-        return ProductRepository.get_products(session)
+    def get_products(cls) -> list[Product]:
+        return ProductRepository.get_products()
     
     @classmethod
-    def get_product(cls, product_id: int, session: Session) -> Product:
-        return ProductRepository.get_product(product_id, session)
+    def get_product(cls, product_id: int) -> Product:
+        return ProductRepository.get_product(product_id)
     
     @classmethod
-    def delete_product(cls, product_id: int, session: Session) -> Product:
-        return ProductRepository.delete_product(product_id, session)
+    def delete_product(cls, product_id: int) -> Product:
+        return ProductRepository.delete_product(product_id)
 
     @classmethod
-    def update_product(cls, product_id: int, product: Product, session: Session) -> Product:
+    def update_product(cls, product_id: int, product: Product) -> Product:
         if product_id != product.id:
             raise HTTPException(status_code = 400, detail = "Product ID does not match")
-        return ProductRepository.update_product(product_id, product, session)
+        return ProductRepository.update_product(product_id, product)
     
     @classmethod
-    def patch_product(cls, product_id: int, product_patch: ProductPatch, session: Session) -> Product:
-        return ProductRepository.patch_product(product_id, product_patch, session)
+    def patch_product(cls, product_id: int, product_patch: ProductPatch) -> Product:
+        return ProductRepository.patch_product(product_id, product_patch)
