778d9745950ee3d97de43e051fe24a2e4d0a8681
Efren Garnica <efrengarnicapalacios@gmail.com>
2025-03-31 14:32:56 -0600

    Aqui hago que fastApi maneje mis excepciones si es que se llegan a presentar. Lo hace mediante la función, que viene por defecto, add_exception_handler.

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ from fastapi import FastAPI
 from app.api.user import router as user_router
 from app.api.product import router as product_router
 from app.api.cart import router as cart_router
-
+from app.exceptions.exceptions import(CartAlreadyRegisteredException, CartItemAlreadyRegisteredException, CartItemNotFoundException, CartNotFoundException, DatabaseIntegrityException, EmailAlreadyRegisteredException, InternalServerErrorException, ProductNameAlreadyExistsException, ProductNotFoundException, UserNotFoundException)
+from app.exceptions.exceptions_handlers import (cart_already_registered_handler, cart_item_already_registered_handler, cart_item_not_found_handler, cart_not_found_handler, database_integrity_exception_handler, email_already_registered_handler, internal_server_error_exception_handler, product_name_already_exists_handler, product_not_found_handler, user_not_found_handler)
 #App principal de FASTAPI
 app = FastAPI()
 
@@ -11,3 +12,15 @@ app = FastAPI()
 app.include_router(user_router, prefix="/users", tags=["users"])
 app.include_router(product_router, prefix="/products", tags=["products"])
 app.include_router(cart_router, prefix="/carts", tags=["carts"])
+
+# Registrar los manejadores globales de excepciones personalizadas
+app.add_exception_handler(UserNotFoundException, user_not_found_handler)
+app.add_exception_handler(EmailAlreadyRegisteredException, email_already_registered_handler)
+app.add_exception_handler(ProductNotFoundException, product_not_found_handler)
+app.add_exception_handler(ProductNameAlreadyExistsException, product_name_already_exists_handler)
+app.add_exception_handler(CartNotFoundException, cart_not_found_handler)
+app.add_exception_handler(CartAlreadyRegisteredException, cart_already_registered_handler)
+app.add_exception_handler(CartItemNotFoundException, cart_item_not_found_handler)
+app.add_exception_handler(CartItemAlreadyRegisteredException, cart_item_already_registered_handler)
+app.add_exception_handler(DatabaseIntegrityException, database_integrity_exception_handler)
+app.add_exception_handler(InternalServerErrorException, internal_server_error_exception_handler)
