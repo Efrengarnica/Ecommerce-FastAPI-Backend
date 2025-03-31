c004a050f7ec46b0cc3542a8245ba9ad7279415c
Efren Garnica <efrengarnicapalacios@gmail.com>
2025-03-31 14:35:01 -0600

    Me aseguro en la capa gateway que no haya IntegrityError o alguna otra excepción que no haya contemplado.

--- app/gateway/cart.py
+++ app/gateway/cart.py
@@ -1,36 +1,52 @@
+from sqlite3 import IntegrityError
 from uuid import UUID
 from fastapi import HTTPException
 from app.models.cart import Cart, CartItem
 from app.schemas.cart import CartItemPatch
 from app.repository.cart import CartRepository
+from app.exceptions.exceptions import (DatabaseIntegrityException, InternalServerErrorException)
 
 class CartGateway:
     
     @classmethod
     def create_cart(cls, cart: Cart) -> Cart:
-        return CartRepository.create_cart(cart)
+        try:
+            return CartRepository.create_cart(cart)
+        except IntegrityError as e:
+            raise DatabaseIntegrityException(str(e))
 
     @classmethod
     def get_carts(cls) -> list[Cart]:
-        return CartRepository.get_carts()
+        try:
+            return CartRepository.get_carts()
+        except Exception as e:
+            raise InternalServerErrorException(str(e))
     
     @classmethod
     def get_cart(cls, cart_id: UUID) -> Cart:
         return CartRepository.get_cart(cart_id)
+     
     
     @classmethod
     def delete_cart(cls, cart_id: UUID) -> Cart:
         return CartRepository.delete_cart(cart_id)
+     
     
     # Métodos para los items del carrito (CartItem)
     @classmethod
     def add_item_to_cart(cls, cart_item: CartItem) -> CartItem:
-        return CartRepository.add_item_to_cart(cart_item)
-       
+        try:
+            return CartRepository.add_item_to_cart(cart_item)
+        except IntegrityError as e:
+            raise DatabaseIntegrityException(str(e))
+        
     @classmethod
     def delete_cart_item(cls, cart_item_id: UUID) -> CartItem:
         return CartRepository.delete_cart_item(cart_item_id)
 
     @classmethod
     def patch_cart_item(cls, cart_item_id: UUID, cart_item_patch: CartItemPatch) -> CartItem:
-        return CartRepository.patch_cart_item(cart_item_id, cart_item_patch) 
+        try: 
+            return CartRepository.patch_cart_item(cart_item_id, cart_item_patch) 
+        except IntegrityError as e:
+            raise DatabaseIntegrityException(str(e))
--- app/gateway/product.py
+++ app/gateway/product.py
@@ -1,30 +1,44 @@
+from sqlite3 import IntegrityError
 from fastapi import HTTPException
 from app.models.product import Product
 from app.schemas.product import ProductPatch
 from app.repository.product import ProductRepository
+from app.exceptions.exceptions import (DatabaseIntegrityException, InternalServerErrorException)
 
 class ProductGateway:
     
     @classmethod
     def create_product(cls, product: Product) -> Product:
-        return ProductRepository.create_product(product)
-
+        try:
+            return ProductRepository.create_product(product)
+        except IntegrityError as e:
+            raise DatabaseIntegrityException(str(e))
+    
     @classmethod
     def get_products(cls) -> list[Product]:
-        return ProductRepository.get_products()
+        try:
+            return ProductRepository.get_products()
+        except Exception as e:
+            raise InternalServerErrorException(str(e))
     
     @classmethod
     def get_product(cls, product_id: int) -> Product:
         return ProductRepository.get_product(product_id)
-    
+      
     @classmethod
     def delete_product(cls, product_id: int) -> Product:
         return ProductRepository.delete_product(product_id)
-
+       
     @classmethod
     def update_product(cls, product: Product) -> Product:
-        return ProductRepository.update_product(product)
+        try:
+            return ProductRepository.update_product(product)
+        except IntegrityError as e:
+            raise DatabaseIntegrityException(str(e))
     
     @classmethod
     def patch_product(cls, product_id: int, product_patch: ProductPatch) -> Product:
-        return ProductRepository.patch_product(product_id, product_patch)
+        try:
+            return ProductRepository.patch_product(product_id, product_patch)
+        except IntegrityError as e:
+            raise DatabaseIntegrityException(str(e))
