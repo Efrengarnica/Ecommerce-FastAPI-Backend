2e48020c5932e3f135c0ab370c7d3c29d7bb45a9
Efren Garnica <efrengarnicapalacios@gmail.com>
2025-03-31 22:57:02 -0600

    Primero me conseguí mis credenciales en la API, después implementas esto para poder guardar tus imagenes en la API y guardar el producto con un link que te dan.

--- app/main.py
+++ app/main.py
@@ -3,8 +3,20 @@ from fastapi import FastAPI
 from app.api.user import router as user_router
 from app.api.product import router as product_router
 from app.api.cart import router as cart_router
-from app.exceptions.exceptions import(CartAlreadyRegisteredException, CartItemAlreadyRegisteredException, CartItemNotFoundException, CartNotFoundException, DatabaseIntegrityException, EmailAlreadyRegisteredException, InternalServerErrorException, ProductNameAlreadyExistsException, ProductNotFoundException, UserNotFoundException)
-from app.exceptions.exceptions_handlers import (cart_already_registered_handler, cart_item_already_registered_handler, cart_item_not_found_handler, cart_not_found_handler, database_integrity_exception_handler, email_already_registered_handler, internal_server_error_exception_handler, product_name_already_exists_handler, product_not_found_handler, user_not_found_handler)
+from app.exceptions.exceptions import(CartAlreadyRegisteredException, CartItemAlreadyRegisteredException, CartItemNotFoundException, CartNotFoundException, CloudinaryUploadException, DatabaseIntegrityException, EmailAlreadyRegisteredException, InternalServerErrorException, ProductNameAlreadyExistsException, ProductNotFoundException, UserNotFoundException)
+from app.exceptions.exceptions_handlers import (cart_already_registered_handler, cart_item_already_registered_handler, cart_item_not_found_handler, cart_not_found_handler, cloudinary_upload_exception_handler, database_integrity_exception_handler, email_already_registered_handler, internal_server_error_exception_handler, product_name_already_exists_handler, product_not_found_handler, user_not_found_handler)
+
+from fastapi.middleware.cors import CORSMiddleware
+
+# pip install cloudinary
+import cloudinary
+#pip install python-dotenv
+import os
+from dotenv import load_dotenv
+
+# Cargar las variables de entorno desde el archivo .env, no se sube al repo
+load_dotenv()
+
 #App principal de FASTAPI
 app = FastAPI()
 
@@ -12,6 +24,24 @@ app = FastAPI()
 app.include_router(user_router, prefix="/users", tags=["users"])
 app.include_router(product_router, prefix="/products", tags=["products"])
 app.include_router(cart_router, prefix="/carts", tags=["carts"])
+
+#Necesario para poder hacer las peticiones a la API, estan en el 
+#archivo .env y las conseguí registrandome en cloudinary.
+cloudinary.config(
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET")
+)
+
+#Esto es para poder permitir peticiones desde otro servidor, es para conectar
+#mi back con mi front me falta implementrarlo.
+app.add_middleware(
+     CORSMiddleware,
+     allow_origins=["http://localhost:5173"],
+     allow_credentials=True,
+     allow_methods=["*"],
+     allow_headers=["*"],
+ )
 
 # Registrar los manejadores globales de excepciones personalizadas
 app.add_exception_handler(UserNotFoundException, user_not_found_handler)
@@ -24,3 +54,4 @@ app.add_exception_handler(CartItemNotFoundException, cart_item_not_found_handler
 app.add_exception_handler(CartItemAlreadyRegisteredException, cart_item_already_registered_handler)
 app.add_exception_handler(DatabaseIntegrityException, database_integrity_exception_handler)
 app.add_exception_handler(InternalServerErrorException, internal_server_error_exception_handler)
+app.add_exception_handler(CloudinaryUploadException, cloudinary_upload_exception_handler)
