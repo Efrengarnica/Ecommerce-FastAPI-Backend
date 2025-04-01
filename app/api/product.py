f86dce02b9e3936ed7604c31812bc4ca7c8b42f2
Efren Garnica <efrengarnicapalacios@gmail.com>
2025-03-31 23:06:58 -0600

    Aqui hago que fastApi maneje mis excepciones si es que se llegan a presentar. Lo hace mediante la función, que viene por defecto, add_exception_handler.

--- app/api/product.py
+++ app/api/product.py
@@ -1,15 +1,17 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter,Depends, File, Form, Request, UploadFile
+
 from app.models.product import Product
 from app.schemas.product import ProductPatch, ProductCreate, ProductPut, ProductResponse
 from app.gateway.product import ProductGateway
 
 router = APIRouter()
-
+    
 @router.post("/", response_model = ProductResponse)
-def create_product(product_create: ProductCreate) -> ProductResponse:
-    product_entity = Product(**product_create.model_dump())
-    created_product = ProductGateway.create_product(product_entity)
+async def create_product(name: str = Form(...), price: float = Form(...), category: str = Form(...),  file: UploadFile = File(...)) -> ProductResponse:
+    product_create = ProductCreate(name=name, price=price, category=category)
+    created_product = ProductGateway.create_product(product_create, file)
     return ProductResponse.model_validate(created_product)
+
 
 @router.get("/", response_model = list[ProductResponse])
 def get_products() -> list[ProductResponse]:
@@ -27,13 +29,18 @@ def delete_product(product_id: int) -> ProductResponse:
     return ProductResponse.model_validate(delete_product)
 
 @router.put("/", response_model = ProductResponse)
-def update_product(product_put: ProductPut) -> ProductResponse:
-    product_entity = Product(**product_put.model_dump())
-    created_product = ProductGateway.update_product(product_entity)
+def update_product(id:int=Form(...), name: str = Form(...), price: float = Form(...), category: str = Form(...), file: UploadFile = File(...)) -> ProductResponse:
+    product_put = ProductPut(id=id, name=name, price=price, category=category)
+    created_product = ProductGateway.update_product(product_put, file)
     return ProductResponse.model_validate(created_product)
 
 @router.patch("/{product_id}", response_model = ProductResponse)
 async def patch_product(request: Request, product_id: int) -> ProductResponse:
     data = await request.json()
     created_product = ProductGateway.patch_product(product_id, ProductPatch(**data))
+    return ProductResponse.model_validate(created_product)
+
+@router.patch("/{product_id}/image", response_model=ProductResponse)
+def patch_product_image(product_id: int, file: UploadFile = File(...)) -> ProductResponse:
+    created_product = ProductGateway.patch_product_image(product_id, file)
     return ProductResponse.model_validate(created_product)
