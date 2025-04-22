a8e37e8c8c5e4e6e23456c2d5bd7b38b29e791a9
Efren Garnica <efrengarnicapalacios@gmail.com>
2025-04-22 11:02:48 -0600

    Ahora que ya puedo realizar migraciones, modifico el modelo de cartItem para que le diga explicitamente a la columna mi bd que acepte zona horaria.

--- app/models/cart.py
+++ app/models/cart.py
@@ -3,6 +3,7 @@ from sqlmodel import SQLModel, Field, Relationship
 from typing import List
 from uuid import UUID, uuid4
 from models.product import Product
+from sqlalchemy import Column, TIMESTAMP
     
 class Cart(SQLModel, table = True):
     id: UUID = Field(default_factory = uuid4, primary_key = True)
@@ -15,6 +16,7 @@ class CartItem(SQLModel, table = True):
     cart_id: UUID = Field(foreign_key = "cart.id")  # Relación con el carrito
     product_id: int = Field(foreign_key = "product.id")  # Relación con el producto
     quantity: int = Field(default = 1, ge = 0)  # Cantidad mínima de 1 y mayor o igual que cero
-    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc),
+    sa_column=Column(TIMESTAMP(timezone=True), nullable=False))
     cart: "Cart" = Relationship(back_populates = "items")  # Relación inversa con el carrito
     product: Product = Relationship()  # Agregamos la relación con el producto
