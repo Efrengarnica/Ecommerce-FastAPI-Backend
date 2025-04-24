facd364ea5cf6a242ed68b4d6b7fe3e4b01ee157
Efren Garnica <efrengarnicapalacios@gmail.com>
2025-04-23 20:05:40 -0600

    Se implementa una clase que ayuda a definir los roles que habrá en la app. Y se agrega el nuevo parámetro a user.

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,12 @@
 from sqlmodel import SQLModel, Field
 from uuid import UUID, uuid4
+from enum import Enum
 
+class Role(str, Enum):
+    client = "client"
+    admin = "admin"
+    root = "root"
+    
 """ Se supone que SQLModel ya trae BaseModel y además esto hace que mi modelo sea una entidad """
 class User(SQLModel, table = True): #Table True hace que sea una tabla en mi base de datos
     id: UUID = Field(primary_key = True, default_factory = uuid4) # Clave primaria
@@ -8,3 +14,4 @@ class User(SQLModel, table = True): #Table True hace que sea una tabla en mi bas
     email: str = Field(index = True, unique = True) # Indexado y único
     age: int | None = None  # Campo opcional
     password: str
+    role: Role = Role.client  # Valor por defecto client
