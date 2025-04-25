95f3e662df034626970cc38a369d0cbf6e262dbb
Efren Garnica <efrengarnicapalacios@gmail.com>
2025-04-24 19:57:17 -0600

    Agrego en mi base.py la variable que me ayudará a traer de mi .env mi variable de ambiente.Esta es necesaria para poder firmar el token.

--- app/settings/base.py
+++ app/settings/base.py
@@ -5,3 +5,6 @@ DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "ecommerce_db")
 DB_USER = os.getenv("DB_USER", "postgres")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "postgres")
+
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY") #Busca en mi local la varible de ambiente que se llame de esa forma, en el .env.
+#Esto es para ver si el contenedor una vez que se levanta reconoce la variable de ambiente de mi local, print(JWT_SECRET_KEY).
