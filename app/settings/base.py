8094b5e8880e3af9251164aa97e3f59895042431
Efren Garnica <efrengarnicapalacios@gmail.com>
2025-04-30 18:35:15 -0600

    Se agrega el test de authorization, un punto importante aqui es que se necesito modificar el archivo base.py en donde tengo mis varibales de entorno ya que el test no las lee y le tengo que decir a este archivo explicitamente en donde puede encontrar el .env y asi cuando se mande a llamar desde el test se encuentre las variables de entorno.

--- app/settings/base.py
+++ app/settings/base.py
@@ -1,5 +1,14 @@
+from pathlib import Path #Ncessario para poder leer el .env en el test
+from dotenv import load_dotenv #Necesario para pode leer el .env en el test
 import os
  
+# settings/base.py
+# 1) Indica la ruta de tu .env (por defecto en el mismo nivel de settings/)
+ENV_PATH = Path(__file__).parent.parent / ".env" #Necesario para poder leer el .env en el test
+
+# 2) Carga las variables de entorno desde ese fichero
+load_dotenv(dotenv_path=ENV_PATH)    # <— lee automáticamente tu .env y lo inyecta en os.environ, neceario para el test.
+
 DB_HOST = os.getenv("DB_HOST", "postgres")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "ecommerce_db")
